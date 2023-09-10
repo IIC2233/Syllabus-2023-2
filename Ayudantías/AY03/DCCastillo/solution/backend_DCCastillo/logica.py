4b9ed268c17ab2e43eec9ebc9d02417e02a21c9d
Camila González <ccgonzalez3@uc.cl>
2023-09-10 15:10:35 -0300

    Solución AY03 :rocket:

---
+++ Ayudantías/AY03/DCCastillo/solution/backend_DCCastillo/logica.py
@@ -0,0 +1,14 @@
+from PyQt6.QtCore import QObject,pyqtSignal
+
+
+class Logica(QObject):
+    
+    senal_ver_dormir = pyqtSignal()
+    
+    def __init__(self):
+        super().__init__()
+        
+    def ver_dormir(self,hora):
+        separado = hora.split(":")
+        if (int(separado[0]) >= 20) or ( int(separado[0]) <= 5 and int(separado[0]) >= 0 ):
+            self.senal_ver_dormir.emit()
