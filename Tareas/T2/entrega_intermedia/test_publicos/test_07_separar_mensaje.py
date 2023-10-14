cfc3980954ba680f82f02cbc45c5310fbd1bfa37
Fran Ibarra <faibarra1@uc.cl>
2023-10-14 17:45:43 -0300

    :ambulance: fix test07 y test08

--- Tareas/T2/entrega_intermedia/test_publicos/test_07_separar_mensaje.py
+++ Tareas/T2/entrega_intermedia/test_publicos/test_07_separar_mensaje.py
@@ -16,7 +16,7 @@ class TestSepararMensaje(unittest.TestCase):
         self.assertIsInstance(respuesta, list)
         for n in respuesta:
             self.assertIsInstance(n, bytearray)
-        self.assertEqual(respuesta, [b"Hauo", b"o n", b"lMd"])
+        self.assertEqual(respuesta, [bytearray(b'HMu'), bytearray(b'o n'), bytearray(b'lado')])
 
     def test_1(self):
         mensaje = bytearray(b"\xc2\xb4+{}[]\xc2\xa8*!#$%&/()")
@@ -25,7 +25,7 @@ class TestSepararMensaje(unittest.TestCase):
         for n in respuesta:
             self.assertIsInstance(n, bytearray)
         self.assertEqual(
-            respuesta, [b"\xc2{]*$/", b"\xb4}\xc2!%(", b"+[\xa8#&)"])
+            respuesta, [bytearray(b'\xc2[]#$)'), bytearray(b'\xb4}\xc2!%('), bytearray(b'+{\xa8*&/')])
 
     def test_2(self):
         mensaje = bytearray(b"A\xc3\xb1o nuevo, vida nueva")
@@ -34,7 +34,7 @@ class TestSepararMensaje(unittest.TestCase):
         for n in respuesta:
             self.assertIsInstance(n, bytearray)
         self.assertEqual(
-            respuesta, [b"Aouovaua", b"\xc3 e,i e", b"\xb1nv dnv"])
+            respuesta, [bytearray(b'Anu vnu'), bytearray(b'\xc3 e,i e'), bytearray(b'\xb1ovodava')])
 
     def test_3(self):
         mensaje = bytearray(b"\xc3\xb1a\xc3\xb1e\xc3\xb1i\xc3\xb1d\xc3\xb1r\xc3"
@@ -45,9 +45,9 @@ class TestSepararMensaje(unittest.TestCase):
         for n in respuesta:
             self.assertIsInstance(n, bytearray)
         self.assertEqual(respuesta, [
-            b"\xc3\xc3\xc3\xc3\xc3\xc3\xc3hhl\xc3\xb4\xc3\xb1\xc2\xb1",
-            b"\xb1\xb1\xb1\xb1\xb1\xb1\xb1\xc3\xc3\xc3\xb1\xc2\xb1\xc2\xb4",
-            b"aeidrtc\xb1\xb1\xb1\xc2\xb4\xc3\xb4\xc3"
+            bytearray(b'\xc3e\xc3d\xc3t\xc3\xb1h\xb1\xc3\xb4\xc3\xb4\xc2'),
+            bytearray(b'\xb1\xb1\xb1\xb1\xb1\xb1\xb1\xc3\xc3\xc3\xb1\xc2\xb1\xc2\xb4'),
+            bytearray(b'a\xc3i\xc3r\xc3ch\xb1l\xc2\xb4\xc3\xb1\xc3\xb1')
         ])
 
     def test_4(self):
@@ -59,10 +59,10 @@ class TestSepararMensaje(unittest.TestCase):
         self.assertIsInstance(respuesta, list)
         for n in respuesta:
             self.assertIsInstance(n, bytearray)
-        self.assertEqual(respuesta, [
-            b"{\xb4]*\xc2\'?\\\xc2^\xa8\xc2\xc2\xc2\xc2\xac$/=\xc2\xc2\xac]",
-            b"}+\xc2^\xa8\xc2\xc2\xc2\xa8`~\xbf\xa1\xb8\xb0!%(|\xb7\xbd{}",
-            b"\xc2[\xa8`~\xbf\xa1\xb8*\xc2\'?\\|\xc2#&)@~\xc2["
+        self.assertEqual(respuesta,  [
+            bytearray(b'{[]`\xc2\xbf?\xb8\xc2\xc2\xa8?\xc2|\xc2#$)=~\xc2[]'),
+            bytearray(b'}+\xc2^\xa8\xc2\xc2\xc2\xa8`~\xbf\xa1\xb8\xb0!%(|\xb7\xbd{}'),
+            bytearray(b"\xc2\xb4\xa8*~\'\xa1\\*^\'\xc2\\\xc2\xc2\xac&/@\xc2\xc2\xac")
         ])
 
     def test_5(self):
@@ -73,9 +73,9 @@ class TestSepararMensaje(unittest.TestCase):
         for n in respuesta:
             self.assertIsInstance(n, bytearray)
         self.assertEqual(respuesta, [
-            b"\xe2\xe2\xc6\x82\x80\xa0\xc5",
-            b"\x80\x80\x92\xac\xb0\xc5\xbd",
-            b"\xa1\xa0\xe2\xe2\xc5\x92"
+            bytearray(b'\xe2\xa0\xc6\xe2\x80\x92\xc5'),
+            bytearray(b'\x80\x80\x92\xac\xb0\xc5\xbd'),
+            bytearray(b'\xa1\xe2\xe2\x82\xc5\xa0')
         ])
 
     def test_6(self):
@@ -85,9 +85,9 @@ class TestSepararMensaje(unittest.TestCase):
         for n in respuesta:
             self.assertIsInstance(n, bytearray)
         self.assertEqual(respuesta, [
-            b"\t\x0b",
-            b"\n\x0c",
-            b"\r"
+            bytearray(b'\t'),
+            bytearray(b'\n\x0c'),
+            bytearray(b'\r\x0b')
         ])
 
     def test_7(self):
@@ -97,34 +97,33 @@ class TestSepararMensaje(unittest.TestCase):
         for n in respuesta:
             self.assertIsInstance(n, bytearray)
         self.assertEqual(respuesta, [
-            b"Hauoo n\nlMd",
-            b"o n\nlMdHauo",
-            b"lMdHauoo n\n"
+            bytearray(b'HMuHounolnd'),
+            bytearray(b'o n\nlMdHauo'),
+            bytearray(b'ladoa o\n M\n')
         ])
 
     def test_8(self):
-        mensaje = bytearray(b"                               ")
+        mensaje = bytearray(b"a               c                b")
         respuesta = separar_mensaje(mensaje)
         self.assertIsInstance(respuesta, list)
         for n in respuesta:
             self.assertIsInstance(n, bytearray)
         self.assertEqual(respuesta, [
-            b"           ",
-            b"          ",
-            b"          "
+            bytearray(b'a          '),
+            bytearray(b'     c     '),
+            bytearray(b'           b')
         ])
 
     def test_9(self):
-        mensaje = bytearray(b"1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1")
+        mensaje = bytearray(b"11 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 1 11")
         respuesta = separar_mensaje(mensaje)
         self.assertIsInstance(respuesta, list)
         for n in respuesta:
             self.assertIsInstance(n, bytearray)
         self.assertEqual(respuesta, [
-            b"1 1 1 1 1 1 1 1",
-            b" 1 1 1 1 1 1 1 ",
-            b"1 1 1 1 1 1 1 1"
-        ])
+            bytearray(b'11 1 1 1 1 1 1 '),
+            bytearray(b'1 1 1 1 1 1 1 11'),
+            bytearray(b' 1 1 1 1 1 1 1 1')])
 
 
 if __name__ == "__main__":
--- Tareas/T2/entrega_intermedia/test_publicos/test_08_encriptar_mensaje.py
+++ Tareas/T2/entrega_intermedia/test_publicos/test_08_encriptar_mensaje.py
@@ -14,21 +14,20 @@ class TestEncriptarMensaje(unittest.TestCase):
         mensaje = bytearray(b"Hola Mundo")
         respuesta = encriptar_mensaje(mensaje)
         self.assertIsInstance(respuesta, bytearray)
-        self.assertEqual(respuesta, bytearray(b"1HauolMdo n"))
+        self.assertEqual(respuesta, bytearray(b'1HMuladoo n'))
 
     def test_1(self):
         mensaje = bytearray(b"\xc2\xb4+{}[]\xc2\xa8*!#$%&/()")
         respuesta = encriptar_mensaje(mensaje)
         self.assertIsInstance(respuesta, bytearray)
-        self.assertEqual(respuesta, bytearray(
-            b"0\xb4}\xc2!%(\xc2{]*$/+[\xa8#&)"))
+        self.assertEqual(respuesta, bytearray(b'0\xb4}\xc2!%(\xc2[]#$)+{\xa8*&/'))
 
     def test_2(self):
         mensaje = bytearray(b"A\xc3\xb1o nuevo, vida nueva")
         respuesta = encriptar_mensaje(mensaje)
         self.assertIsInstance(respuesta, bytearray)
         self.assertEqual(respuesta, bytearray(
-            b"0\xc3 e,i eAouovaua\xb1nv dnv"))
+            b'0\xc3 e,i eAnu vnu\xb1ovodava'))
 
     def test_3(self):
         mensaje = bytearray(b"\xc3\xb1a\xc3\xb1e\xc3\xb1i\xc3\xb1d\xc3\xb1r\xc3"
@@ -37,9 +36,9 @@ class TestEncriptarMensaje(unittest.TestCase):
         respuesta = encriptar_mensaje(mensaje)
         self.assertIsInstance(respuesta, bytearray)
         self.assertEqual(respuesta, bytearray(
-            b"1\xc3\xc3\xc3\xc3\xc3\xc3\xc3hhl\xc3\xb4\xc3\xb1"
-            b"\xc2\xb1aeidrtc\xb1\xb1\xb1\xc2\xb4\xc3\xb4\xc3"
-            b"\xb1\xb1\xb1\xb1\xb1\xb1\xb1\xc3\xc3\xc3\xb1\xc2\xb1\xc2\xb4"))
+            b"1\xc3e\xc3d\xc3t\xc3\xb1h\xb1\xc3\xb4\xc3\xb4\xc2a\xc3i\xc3r"
+            b"\xc3ch\xb1l\xc2\xb4\xc3\xb1\xc3\xb1\xb1\xb1\xb1\xb1\xb1\xb1"
+            b"\xb1\xc3\xc3\xc3\xb1\xc2\xb1\xc2\xb4"))
 
     def test_4(self):
         mensaje = bytearray(
@@ -49,9 +48,10 @@ class TestEncriptarMensaje(unittest.TestCase):
         respuesta = encriptar_mensaje(mensaje)
         self.assertIsInstance(respuesta, bytearray)
         self.assertEqual(respuesta, bytearray(
-            b"1{\xb4]*\xc2\'?\\\xc2^\xa8\xc2\xc2\xc2\xc2\xac$/=\xc2\xc2\xac]"
-            b"\xc2[\xa8`~\xbf\xa1\xb8*\xc2\'?\\|\xc2#&)@~\xc2[}+\xc2^\xa8"
-            b"\xc2\xc2\xc2\xa8`~\xbf\xa1\xb8\xb0!%(|\xb7\xbd{}"))
+            b"1{[]`\xc2\xbf?\xb8\xc2\xc2\xa8?\xc2|\xc2#$)=~"
+            b"\xc2[]\xc2\xb4\xa8*~\'\xa1\\*^\'\xc2\\\xc2\xc2"
+            b"\xac&/@\xc2\xc2\xac}+\xc2^\xa8\xc2\xc2\xc2\xa8`~"
+            b"\xbf\xa1\xb8\xb0!%(|\xb7\xbd{}"))
 
     def test_5(self):
         mensaje = bytearray(
@@ -59,32 +59,31 @@ class TestEncriptarMensaje(unittest.TestCase):
         respuesta = encriptar_mensaje(mensaje)
         self.assertIsInstance(respuesta, bytearray)
         self.assertEqual(respuesta, bytearray(
-            b"1\xe2\xe2\xc6\x82\x80\xa0\xc5\xa1\xa0\xe2\xe2\xc5\x92\x80\x80\x92\xac\xb0\xc5\xbd"))
+            b'1\xe2\xa0\xc6\xe2\x80\x92\xc5\xa1\xe2\xe2\x82\xc5\xa0\x80\x80\x92\xac\xb0\xc5\xbd'))
 
     def test_6(self):
         mensaje = bytearray(b"\xc2\xb4+{}[]\xc2\xa8*!#$%&/()")
         respuesta = encriptar_mensaje(mensaje)
         self.assertIsInstance(respuesta, bytearray)
-        self.assertEqual(respuesta, bytearray(
-            b"0\xb4}\xc2!%(\xc2{]*$/+[\xa8#&)"))
+        self.assertEqual(respuesta, bytearray(b'0\xb4}\xc2!%(\xc2[]#$)+{\xa8*&/'))
 
     def test_7(self):
-        mensaje = bytearray(b"   ")
+        mensaje = bytearray(b"a b c")
         respuesta = encriptar_mensaje(mensaje)
         self.assertIsInstance(respuesta, bytearray)
-        self.assertEqual(respuesta, bytearray(b"1   "))
+        self.assertEqual(respuesta, bytearray(b'1ab  c'))
 
     def test_8(self):
-        mensaje = bytearray(b"1 1 1 1 1 1 1 1 1 1 1 1")
+        mensaje = bytearray(b"11 1 1 1 1 1 1 1 1 1 1 11")
         respuesta = encriptar_mensaje(mensaje)
         self.assertIsInstance(respuesta, bytearray)
-        self.assertEqual(respuesta, bytearray(b"0 1 1 1 11 1 1 1 1 1 1 1"))
+        self.assertEqual(respuesta, bytearray(b'01 1 1 1 11 1 1 11 1 1 1 1'))
 
     def test_9(self):
         mensaje = bytearray(b"abcdefghijklmnopqrstuvwxyz")
         respuesta = encriptar_mensaje(mensaje)
         self.assertIsInstance(respuesta, bytearray)
-        self.assertEqual(respuesta, bytearray(b"1adgjmpsvycfiloruxbehknqtwz"))
+        self.assertEqual(respuesta, bytearray(b'1afglmrsxycdijopuvbehknqtwz'))
 
 
 if __name__ == "__main__":
